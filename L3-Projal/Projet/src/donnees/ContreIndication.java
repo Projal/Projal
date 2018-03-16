ffb46cf855c4e99a9a74c802de3ee83d1bdb0338
Projal <37215335+Projal@users.noreply.github.com>
2018-03-16 13:41:29 +0100

    Add files via upload

---
+++ L3-Projal/Projet/src/donnees/ContreIndication.java
@@ -0,0 +1,13 @@
+package donnees;
+
+public class ContreIndication {
+	private String nom;
+
+	public ContreIndication(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+}
