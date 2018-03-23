2931e6fd0c2894cb94a9da48cc5b46dcb2b84510
Projal <37215335+Projal@users.noreply.github.com>
2018-03-23 12:25:47 +0100

    Update projal.sql

--- projal.sql
+++ projal.sql
@@ -21,20 +21,20 @@ DROP TABLE Maladie;
 
 
 CREATE TABLE Medecin(
-        id     Int NOT NULL ,
-        nom    Varchar (25) NOT NULL ,
-        prenom Varchar (25) NOT NULL ,
-        mdp    Varchar (25) NOT NULL ,
-        mail   Varchar (25) NOT NULL ,
-        PRIMARY KEY (id )
+        id_medecin   Int NOT NULL ,
+        nom    Varchar (100) NOT NULL ,
+        prenom Varchar (100) NOT NULL ,
+        mdp    Varchar (100) NOT NULL ,
+        mail   Varchar (100) NOT NULL ,
+        PRIMARY KEY (id_medecin)
 );
 
 
 
 CREATE TABLE Patient(
         id_patient     Int NOT NULL ,
-        nom_patient    Varchar (25) NOT NULL ,
-        prenom_patient Varchar (25) NOT NULL ,
+        nom_patient    Varchar (100) NOT NULL ,
+        prenom_patient Varchar (100) NOT NULL ,
         cat_age        Int NOT NULL ,
         PRIMARY KEY (id_patient )
 );
@@ -44,18 +44,18 @@ CREATE TABLE Patient(
 CREATE SEQUENCE id_trait;
 
 CREATE TABLE Traitement(
-        id_trait   int  NOT NULL ,
+        id_trait   NUMBER  NOT NULL ,
         dateD      Date NOT NULL ,
         DateF      Date NOT NULL ,
-        id_maladie Int ,
+        id_maladie NUMBER not null,
         PRIMARY KEY (id_trait)
 );
 
 CREATE SEQUENCE id_medic;
 
 CREATE TABLE Medicament(
-        id_medic  int NOT NULL ,
-        nom_medic Varchar (25) NOT NULL ,
+        id_medic  NUMBER NOT NULL ,
+        nom_medic Varchar (100) NOT NULL ,
         id_molec  Int NOT NULL ,
         PRIMARY KEY (id_medic )
 );
@@ -64,19 +64,20 @@ CREATE TABLE Medicament(
 CREATE SEQUENCE id_molec;
 
 CREATE TABLE Molecule(
-        id_molec  int NOT NULL ,
-        nom_molec Varchar (25) NOT NULL ,
-        conflit   Int NOT NULL ,
-        PRIMARY KEY (id_molec ) ,
-        UNIQUE (nom_molec )
+        id_molec  NUMBER NOT NULL ,
+        nom_molec Varchar (100) NOT NULL ,
+        type_molec varchar (100) NOT NULL,
+        conflit varchar(100) ,
+        PRIMARY KEY (id_molec) ,
+        UNIQUE (nom_molec)
 );
 
 
 CREATE SEQUENCE id_contr;
 
 CREATE TABLE Contre_indication(
-        id_contr  int NOT NULL ,
-        nom_contr Varchar (25) NOT NULL ,
+        id_contr  NUMBER NOT NULL ,
+        nom_contr Varchar (100) NOT NULL ,
         PRIMARY KEY (id_contr )
 );
 
@@ -85,24 +86,24 @@ CREATE SEQUENCE id_maladie;
 
 
 CREATE TABLE Maladie(
-        id_maladie  int NOT NULL ,
-        nom_maladie Varchar (25) NOT NULL ,
+        id_maladie  NUMBER NOT NULL ,
+        nom_maladie Varchar (100) NOT NULL ,
         PRIMARY KEY (id_maladie )
 );
 
 
 
 CREATE TABLE Prescrit(
-        id         Int NOT NULL ,
+        id_medecin Int NOT NULL ,
         id_patient Int NOT NULL ,
-        id_trait   Int NOT NULL ,
-        PRIMARY KEY (id ,id_patient ,id_trait )
+        id_trait   NUMBER NOT NULL ,
+        PRIMARY KEY (id_medecin,id_patient ,id_trait )
 );
 
 
 
 CREATE TABLE EstAssocie(
-        id_contr   Int NOT NULL ,
+        id_contr   NUMBER NOT NULL ,
         id_patient Int NOT NULL ,
         PRIMARY KEY (id_contr ,id_patient )
 );
@@ -110,9 +111,9 @@ CREATE TABLE EstAssocie(
 
 
 CREATE TABLE EstConstitue(
-        id_trait Int NOT NULL ,
-        id_medic Int NOT NULL ,
-        PRIMARY KEY (id_trait ,id_medic )
+        id_trait NUMBER NOT NULL ,
+        id_medic NUMBER NOT NULL ,
+        PRIMARY KEY (id_trait ,id_medic)
 );
 
 
@@ -122,8 +123,6 @@ CREATE TABLE Concerne(
         PRIMARY KEY (id_molec ,id_contr )
 );
 
-
-
 CREATE TABLE Soigne(
         id_molec   Int NOT NULL ,
         id_maladie Int NOT NULL ,
@@ -132,7 +131,7 @@ CREATE TABLE Soigne(
 
 ALTER TABLE Traitement ADD CONSTRAINT FK_Traitement_id_maladie FOREIGN KEY (id_maladie) REFERENCES Maladie(id_maladie);
 ALTER TABLE Medicament ADD CONSTRAINT FK_Medicament_id_molec FOREIGN KEY (id_molec) REFERENCES Molecule(id_molec);
-ALTER TABLE Prescrit ADD CONSTRAINT FK_Prescrit_id FOREIGN KEY (id) REFERENCES Medecin(id);
+ALTER TABLE Prescrit ADD CONSTRAINT FK_Prescrit_id FOREIGN KEY (id_medecin) REFERENCES Medecin(id_medecin);
 ALTER TABLE Prescrit ADD CONSTRAINT FK_Prescrit_id_patient FOREIGN KEY (id_patient) REFERENCES Patient(id_patient);
 ALTER TABLE Prescrit ADD CONSTRAINT FK_Prescrit_id_trait FOREIGN KEY (id_trait) REFERENCES Traitement(id_trait);
 ALTER TABLE EstAssocie ADD CONSTRAINT FK_EstAssocie_id_contr FOREIGN KEY (id_contr) REFERENCES Contre_indication(id_contr);
