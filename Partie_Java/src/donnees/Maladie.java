74afdbe8b347a6fd384d36c16ae76a2c5921aa82
Projal <37215335+Projal@users.noreply.github.com>
2018-03-23 19:01:15 +0100

    Update Maladie.java

--- Partie_Java/src/donnees/Maladie.java
+++ Partie_Java/src/donnees/Maladie.java
@@ -1,14 +1,20 @@
 package donnees;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class Maladie {
 	private String nom;
 	private List<Molecule> molecule;
 	
-	public Maladie(String nom, List<Molecule> molecule) {
+	public Maladie(Statement stmt, String nom, List<Molecule> molecule) throws SQLException {
 		this.nom = nom;
 		this.molecule = molecule;
+		if(!maladieExistante(stmt, nom)) {
+			stmt.executeQuery("INSERT INTO Maladie VALUES(id_maladie.nextval,'"+nom+"')");
+		}
 	}
 	
 	public String getNom() {
@@ -16,5 +22,15 @@ public class Maladie {
 	}
 	public List<Molecule> getMolecule() {
 		return molecule;
+	}
+	
+	public boolean maladieExistante(Statement stmt, String nom_maladie) throws SQLException {
+		ResultSet result = stmt.executeQuery("SELECT NOM_MALADIE FROM MALADIE WHERE NOM_MALADIE = '" +nom_maladie+ "'");
+		if(result.next()){
+		if(result.getString(1).equals(nom_maladie))
+			return true;
+		return false;
+		}
+		return false;
 	}
 }
